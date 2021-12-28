18d8c39d6c4bf33ae4870349e30d95954d24d86b
yangyi <yangyi7351598@gmail.com>
2021-12-28 18:57:06 +0800

    linkage增加删除按钮

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -5,7 +5,12 @@
     <el-table-column prop="link" label="链接" width="300"> </el-table-column>
     <el-table-column prop="describe" label="描述" width="180"></el-table-column>
     <el-table-column label="操作" width="180">
-      <el-button type="success" plain size="mini">复制</el-button>
+      <template slot-scope="scope">
+        <el-button type="success" plain size="mini">复制</el-button>
+        <el-button @click="del(scope.row.id)" type="danger" plain size="mini"
+          >删除</el-button
+        >
+      </template>
     </el-table-column>
   </el-table>
 </template>
@@ -17,19 +22,28 @@ export default {
       links: [],
     };
   },
-  // created() {
-  //   this.$http
-  //     .get("http://rap2api.taobao.org/app/mock/data/2146859")
-  //     .then((res) => {
-  //       this.links = res.data.result;
-  //     });
-  // },
-    created() {
-    this.$http
-      .get("http://yycode.com.cn:8030/linkage/list")
-      .then((res) => {
-        this.links = res.data;
-      });
+  // 不要在created中执行初始化方法,尽量用mounted()
+  mounted() {
+    this.$http.get("http://yycode.com.cn:8030/linkage/list").then((res) => {
+      this.links = res.data;
+    });
+  },
+  methods: {
+    del(id) {
+      this.$http
+        .get("http://localhost:8030/linkage/del?id=" + id)
+        .then((res) => {
+          this.res = res.data;
+          if (res.data == true) {
+            alert("删除成功");
+          }
+          this.$http
+            .get("http://yycode.com.cn:8030/linkage/list")
+            .then((res) => {
+              this.links = res.data;
+            });
+        });
+    },
   },
 };
 </script>
